b609ec6dae801d575c0165d13235ab03870e67ac
Kylian <Kylian.Bongertman@topicus.nl>
2023-02-10 15:32:56 +0000

    nieuwe output tekst test

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "I'm using canary deployments",
+            "message": "Ik gebruik canary deployments",
             # "location": ip.text.replace("\n", "")
         }),
     }
